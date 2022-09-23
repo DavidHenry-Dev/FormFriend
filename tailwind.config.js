5d3b385365672cbfce14754c2bb5047ab0329c54
David <davidhenrydev@gmail.com>
2022-09-22 20:46:16 -0400

    styled index, header, and nav. Tailwind woorking with ejs and partials

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/partials/*.ejs', './views/*.ejs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
