ddceff26744af042288c960862706a5e9ceded52
David Henry <davidhenrydev@gmail.com>
2023-01-09 16:06:45 -0500

    enabled large file uploads with cloudinary fix7

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //Body Parsing
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb'}));
 
 //Logging
 app.use(logger('dev'));
