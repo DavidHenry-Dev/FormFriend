319ef97eae36ec095f417dc4e4cf6eef74a903c1
David Henry <davidhenrydev@gmail.com>
2023-01-10 12:33:12 -0500

    large video upload fix

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,22 @@
 const multer = require('multer');
 const path = require('path');
 
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'video/mp4' || file.mimetype === 'video/quicktime') {
+      cb(null, true);
+    } else {
+      cb(new Error('File type is not supported'), false);
+      return;
+    }
+  },
+  limits: {
+    fieldSize: '50mb'
+ },
+ chunk_size: 6000000
+});
+
 // module.exports = multer({
 //   storage: multer.diskStorage({}),
 //   fileFilter: (req, file, cb) => {
@@ -15,17 +31,3 @@ const path = require('path');
 //     fieldSize: '50mb'
 //  }
 // });
-
-module.exports = multer({
-  storage: videoStorage,
-  limits: {
-  fileSize: 50000000 // 10000000 Bytes = 50 MB
-  },
-  fileFilter(req, file, cb) {
-    // upload only mp4 and mkv format
-    if (!file.originalname.match(/\.(mp4|MPEG-4|mkv|MOV)$/)) { 
-       return cb(new Error('Please upload a video'))
-    }
-    cb(undefined, true)
- }
-})
