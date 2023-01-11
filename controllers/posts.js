113b9688440554ce45331811e2b8821f92e36ce4
David Henry <davidhenrydev@gmail.com>
2023-01-11 14:01:39 -0500

    testing fixes

--- controllers/posts.js
+++ controllers/posts.js
@@ -42,7 +42,7 @@ module.exports = {
       const upload = await cloudinary.uploader.upload(req.file.path, {
         resource_type: 'video', 
         folder: 'FormFriend/vidUploads',
-        quality: 'auto:eco'
+        fetch_format: 'auto:eco'
       });
 
       await Post.create({
