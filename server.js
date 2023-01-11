6cea0bc172ca84356e6cbb404ac8d769d0ba1daf
David Henry <davidhenrydev@gmail.com>
2023-01-11 11:09:56 -0500

    testing fixes

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static('public'));
 
 //Body Parsing
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 
 //Logging
 app.use(logger('dev'));
