d7c53a03f92e1adf72300c0b75617ba53deb3838
David Henry <davidhenrydev@gmail.com>
2023-01-13 13:59:39 -0500

    testing fixes

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,7 +3,11 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 require('dotenv').config({ path: './config/.env' });
 
-cloudinary.config({ path: './config/.env' });
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 
 const storage = new CloudinaryStorage({
