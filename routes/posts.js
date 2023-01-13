19b17e7caab0b01e03df3004a9b84e446ea567a5
David Henry <davidhenrydev@gmail.com>
2023-01-13 11:09:03 -0500

    testing fixes

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middleware/multer');
 const postsController = require('../controllers/posts');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
-const upload = require('parser');
-
-
 
 //Post Routes - simplified for now
 router.get('/:id', ensureAuth, postsController.getPost);
