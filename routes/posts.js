1d9f3266d46b8d2bb723834fa09a8a7342967f74
David <davidhenrydev@gmail.com>
2023-01-16 12:19:28 -0500

    fix for file size limit

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,7 @@ const upload = require('../middleware/multer');
 //Post Routes - simplified for now
 router.get('/:id', ensureAuth, postsController.getPost);
 
-router.post('/createPost', upload.single('video'), postsController.createPost);
+router.post('/createPost', upload.single('file'), postsController.createPost);
 
 // router.put('/likePost/:id', postsController.likePost)
 
