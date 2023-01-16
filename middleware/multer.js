3e7da5036f3452d36a274edcb099b6d864e801b9
David <davidhenrydev@gmail.com>
2023-01-16 18:18:22 -0500

    fix for file size limit

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,6 @@
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const path = require("path");
 
 
 const storage = new CloudinaryStorage({
