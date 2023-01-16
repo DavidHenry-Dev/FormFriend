00b0c7861d4134b91272babd1c3ecfea271994d6
David <davidhenrydev@gmail.com>
2023-01-16 18:35:45 -0500

    fix for file size limit

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,31 +3,31 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-      folder: 'FormFriend/vidUploads',
-      format: 'mp4',
-      quality: 'auto:eco',
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-const upload = multer({ storage });
-
-module.exports = upload;
-
-
-// module.exports = multer({
-//   storage: multer.diskStorage({}),
-//   fileFilter: (req, file, cb) => {
-//     if (file.mimetype === 'video/mp4' || file.mimetype === 'video/quicktime') {
-//       cb(null, true);
-//     } else {
-//       cb(new Error('File type is not supported'), false);
-//       return;
-//     }
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//       folder: 'FormFriend/vidUploads',
+//       format: 'mp4',
+//       quality: 'auto:eco',
 //   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   }
 // });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
+
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'video/mp4' || file.mimetype === 'video/quicktime') {
+      cb(null, true);
+    } else {
+      cb(new Error('File type is not supported'), false);
+      return;
+    }
+  },
+});
